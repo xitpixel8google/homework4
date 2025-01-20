56c11c5d79a75fe912f17942e5c9695844ba8563
xitpixel8google <xitpixel8@gmail.com>
2025-01-20 23:15:40 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,52 @@
+// "use strict"
+
+
+// 1
+
+// function dataFunc(name, data, modernData) {
+//     console.log(`${name}, Ваш возраст ${modernData - data}`)
+// }
+
+
+// 2
+
+// function mathFunc(x) {
+//     for (let i = 0; i < x; i++) {
+//         let min = Math.ceil(1);
+//         let max = Math.floor(99);
+//         let num1 = Math.floor(Math.random() * (max - min + min) + min)
+//         let num2 = Math.floor(Math.random() * (max - min + min) + min)
+//         let operator = Math.floor(Math.random() * (4 - 1 + 1) + 1)
+
+//         if (operator == 1) {
+//             let work = prompt(`${num1} + ${num2}`)
+//             if (work == (num1 + num2)) {
+//                 alert('Success')
+//             } else {
+//                 alert('Error')
+//             }
+//         } else if (operator == 2) {
+//             let work = prompt(`${num1} - ${num2}`)
+//             if (work == (num1 - num2)) {
+//                 alert('Success')
+//             } else {
+//                 alert('Error')
+//             }
+//         } else if (operator == 3) {
+//             let work = prompt(`${num1} * ${num2}`)
+//             if (work == (num1 * num2)) {
+//                 alert('Success')
+//             } else {
+//                 alert('Error')
+//             }
+//         } else if (operator == 4) {
+//             let work = prompt(`${num1} / ${num2}`)
+//             if (work == (num1 / num2)) {
+//                 alert('Success')
+//             } else {
+//                 alert('Error')
+//             }
+//         }
+//     }
+// }
+
